a41d87dab13d73ed35f933237c222836cf986652
Ahmedalihh <ahmedalibbb227@gmail.com>
2024-12-12 17:10:10 +0500

    my game

---
+++ index.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import { match } from "assert";
+
+//to generate a secret number
+const secretNumber: number = Math.floor(Math.random() * 10);
+
+console.log(chalk.bgGreenBright.redBright("No Guessing Game:"));
+// console.log(secretNumber);
+
+// user input
+let userInput = await inquirer.prompt({
+  type: "input",
+  name: "num",
+  message: "please Enter the number between 1-10 :"
+});
+
+// converting user input into number
+
+let num1: number = parseInt(userInput.num);
+
+// switch case
+switch (true) {
+  case num1 === secretNumber:
+    console.log(chalk.bold.green("you won"));
+    break;
+  default:
+    console.log(chalk.bold.red("better luck next time"));
+}
